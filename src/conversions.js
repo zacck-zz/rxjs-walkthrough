2a95d84265d43d3f292d94bf32ceb9a97f2804ed
zacck <coderv63@gmail.com>
2017-09-19 14:10:23 +0200

    add labels to console logs

--- src/conversions.js
+++ src/conversions.js
@@ -2,6 +2,9 @@ var Rx = require('rxjs');
 
 const ofObz = Rx.Observable.of('foo', 'bar');
 
-ofObz.subscribe(x => console.log(x));
+ofObz.subscribe(x => console.log('of Observer',x));
 
+const frmObz = Rx. Observable.from([1,2,4,5]);
+
+frmObz.subscribe(x => console.log('from Observer',x));
 
